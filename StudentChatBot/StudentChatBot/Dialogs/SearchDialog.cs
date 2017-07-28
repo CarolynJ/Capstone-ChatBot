22cebd5b8c3f33a7de481d96b6a830896d0d36ae
Stacy Harrison <stacy.e.harrison@gmail.com>
2017-07-28 14:12:29 -0400

    integrated luis api calls

--- StudentChatBot/StudentChatBot/Dialogs/SearchDialog.cs
+++ StudentChatBot/StudentChatBot/Dialogs/SearchDialog.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using StudentChatBot.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,7 +23,7 @@ namespace StudentChatBot.Dialogs
 
         public Task StartAsync(IDialogContext context)
         {
-            context.PostAsync("What are you looking for today?");
+            context.PostAsync("What are you looking for today? (type exit when you're done searching)");
             context.Wait(MessageReceivedAsync);
 
             return Task.CompletedTask;
@@ -30,15 +32,32 @@ namespace StudentChatBot.Dialogs
         public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
         {
             var activity = await result;
-            await context.PostAsync("you said: " + activity.Text.ToString());
-            var thing = LuisDecipher(activity.Text);
 
-            //await context.PostAsync(luisResponse.Text);
+            if (activity.Text.ToLower().Contains("exit"))
+            {
+                context.Done(true);
+            }
+            else
+            {
+                LuisData data = await LuisDecipher(activity.Text);
+
+                if (data.entities.Length == 0)
+                {
+                    await context.PostAsync("Sorry, I didn't understand that. Try saying, 'Help me with OOP'");
+                }
+                else
+                {
+                    await context.PostAsync(data.entities[0].entity);
+                }
+            }
         }
 
-        private static async Task<string> LuisDecipher(string text)
+
+
+        private async Task<LuisData> LuisDecipher(string text)
         {
             text = Uri.EscapeDataString(text);
+            LuisData data = new LuisData();
 
             using (HttpClient client = new HttpClient())
             {
@@ -48,13 +67,11 @@ namespace StudentChatBot.Dialogs
                 if(msg.IsSuccessStatusCode)
                 {
                     var jsonDataResponse = await msg.Content.ReadAsStringAsync();
-                    return jsonDataResponse;
-                }
-
-                return string.Empty;
-                
-
+                    data = JsonConvert.DeserializeObject<LuisData>(jsonDataResponse);
+                }         
             }
+
+            return data;
         }
     }
 }
