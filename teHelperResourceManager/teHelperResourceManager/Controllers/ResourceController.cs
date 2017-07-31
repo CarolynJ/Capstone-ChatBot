6d194bef434ff55ed7212703ffb73551512e54a9
Stacy Harrison <stacy.e.harrison@gmail.com>
2017-07-31 10:54:31 -0400

    split homecontroller into resource and keyword controllers

--- teHelperResourceManager/teHelperResourceManager/Controllers/ResourceController.cs
+++ teHelperResourceManager/teHelperResourceManager/Controllers/ResourceController.cs
@@ -3,15 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using teHelperResourceManager.DAL;
+using teHelperResourceManager.Models;
 
 namespace teHelperResourceManager.Controllers
 {
     public class ResourceController : Controller
     {
-        // GET: Resource
+        IKeywordSource keywordDal;
+        IResourceSource resourceDal;
+
+        public ResourceController(IKeywordSource keywordDal, IResourceSource resourceDal)
+        {
+            this.keywordDal = keywordDal;
+            this.resourceDal = resourceDal;
+        }
+
         public ActionResult Index()
         {
             return RedirectToAction("Index", "Home");
+        }
+
+
+        public ActionResult AddResource()
+        {
+            NewResourceKeywordViewModel model = new NewResourceKeywordViewModel();
+
+            return View("AddResource", model);
+        }
+
+        [HttpPost]
+        public ActionResult AddResource(NewResourceKeywordViewModel model)
+        {
+            Resource r = new Resource()
+            {
+                ResourceContent = model.newResource.ResourceContent,
+                ResourceTitle = model.newResource.ResourceTitle,
+                PathwayResource = model.newResource.PathwayResource
+            };
+
+            List<string> newKeywordStrings = model.keywordsAsString.Split(',').Select(str => str.Trim()).ToList();
+            List<Keywords> newKeywords = new List<Keywords>();
+
+            foreach (string kw in newKeywordStrings)
+            {
+                if (keywordDal.DoesKeywordAlreadyExist(kw) == null)
+                {
+                    keywordDal.SaveNewKeyword(new Keywords() { Keyword = kw });
+                }
+
+                newKeywords.Add(keywordDal.DoesKeywordAlreadyExist(kw));
+            }
+
+            bool successfullyAddedResource = keywordDal.AddKeywordsToOneResource(newKeywords, r);
+
+            if (successfullyAddedResource)
+            {
+                TempData["NewResource_Success"] = true;
+            }
+
+            return RedirectToAction("Index", "Home");
+
         }
     }
 }
