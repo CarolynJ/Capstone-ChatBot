02fa2a52b7209d2cbf9b3cc36a2e356294aac110
Kevin Somerville <ksomerville0101@gmail.com>
2017-08-02 15:29:55 -0400

    css

--- StudentChatBot/StudentChatBot/Dialogs/MatchmakingDialog.cs
+++ StudentChatBot/StudentChatBot/Dialogs/MatchmakingDialog.cs
@@ -9,12 +9,42 @@ namespace StudentChatBot.Dialogs
 {
     public class MatchmakingDialog : IDialog<object>
     {
+        private const string Schedule = "Matchmaking Schedule";
+        private const string Reminder = "Get a Reminder";
+        private const string Exit = "Exit";
+
         public async Task StartAsync(IDialogContext context)
         {
             await context.PostAsync("Yay it's matchmaking time, check your schedule or set a reminder to follow up with an employer.");
 
-            //this.ShowMatchmakingMenu(context);
+            this.ShowMatchmakingOptions(context);
         }
+
+        private void ShowMatchmakingOptions(IDialogContext context)
+        {
+            PromptDialog.Choice(context, this.BrowseOptions, new List<string>()
+            { Schedule, Reminder, Exit},
+            " ",
+                "Hmm, I didn't understand that, try again.",
+                2);
+        }
+
+        private async Task BrowseOptions(IDialogContext context, IAwaitable<string> result)
+        {
+            var optionSelected = await result;
+
+            switch (optionSelected)
+            {
+                //case Schedule
+
+                case Exit:
+                    context.Done(true);
+                    break;
+
+            }
+        }
+
+
 
     }
 }
