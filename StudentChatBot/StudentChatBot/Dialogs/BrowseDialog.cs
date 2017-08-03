f70235802cf51e61773fd62e7fc736b062b6ec25
Carolyn Jillson <carolynj70@gmail.com>
2017-08-03 16:14:27 -0400

    help dialog, some final fixes

--- StudentChatBot/StudentChatBot/Dialogs/BrowseDialog.cs
+++ StudentChatBot/StudentChatBot/Dialogs/BrowseDialog.cs
@@ -12,8 +12,8 @@ namespace StudentChatBot.Dialogs
     {
         private const string Pathway = "Pathway Resources";
         private const string Technical = "Technical Resources";
-        private const string MainMenu = "Main Menu";
-        
+        private const string OtherOption = "Other";
+        private const string ExitOption = "Exit";
 
         public async Task StartAsync(IDialogContext context)
         {
@@ -24,8 +24,8 @@ namespace StudentChatBot.Dialogs
         private void ShowBrowseOptions(IDialogContext context)
         {
             PromptDialog.Choice(context, this.BrowseOptions, new List<string>()
-            { Pathway, Technical, MainMenu},
-            " ",
+            { Pathway, Technical, OtherOption, ExitOption },
+                " ",
                 "Hmm, I didn't understand that, try again.",
                 2);
         }
@@ -42,8 +42,12 @@ namespace StudentChatBot.Dialogs
                 case Technical:
                     context.Call(new TechnicalDialog(), this.ResumeAfterBrowse);
                     break;
-                //case MainMenu:
-                //    context.Done(true);
+                case OtherOption:
+                    context.Call(new SearchDialog(), this.ResumeAfterBrowse);
+                    break;
+                case ExitOption:
+                    context.Done(true);
+                    break;
 
             }
         }
--- database/SQLQuery3.sql
+++ database/SQLQuery3.sql
@@ -7,9 +7,14 @@ values ('Trust the process.', 'Andria Loczi', 'https://static1.squarespace.com/s
 ('When the aters get rough, keep your oar in the water.', 'CB', 'https://static1.squarespace.com/static/55ef2da9e4b03f6e1ef0cd28/t/57dc62f8ebbd1a8e8340a00f/1474061055358/d9162d20-13cb-4b0c-892b-1dafb08b6b00.png?format=750w');
 
 insert into Motivation
-values('Whatever is meant to happen will happen.', 'Andria Loczi', 'https://static1.squarespace.com/static/55ef2da9e4b03f6e1ef0cd28/t/57d3125546c3c4fb955efa21/1473450614923/?format=500w'),
+values
+('Frustration is part of the process', 'Joe Erickson', 'https://static1.squarespace.com/static/55ef2da9e4b03f6e1ef0cd28/t/58c1cf1829687f4f84578e13/1489096654083/ddfd6120-b5cf-4752-bd88-31f1fb1d389f.png?format=750w');
+('The best part of working at tech Elevator is that I can carry a small part of every student in my heart.', 'Carole Ryglewicz', 'https://static1.squarespace.com/static/55ef2da9e4b03f6e1ef0cd28/t/57a4da4bbebafb5eab53f3cb/1470421591941/?format=750w'),
+('The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart', 'Helen Keller', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNRKnvjz6OcwRHFNxKopvJNcDorEmVkz0GZE31avJuTvyIz0Vn');
+('Whatever is meant to happen will happen.', 'Andria Loczi', 'https://static1.squarespace.com/static/55ef2da9e4b03f6e1ef0cd28/t/57d3125546c3c4fb955efa21/1473450614923/?format=500w'),
 ('Why don''t you build a spreadsheet instead.', 'Sam Learner', 'https://static1.squarespace.com/static/55ef2da9e4b03f6e1ef0cd28/t/57bf0be8cd0f6879f01f02e4/1497033395481/68a925bf-61eb-4fed-993a-4dbf13bb899f.png?format=750w'),
 ('Take a break and try some of my homemade hummus.', 'Carole Ryglewicz', 'https://static1.squarespace.com/static/55ef2da9e4b03f6e1ef0cd28/t/57a4da4bbebafb5eab53f3cb/1470421591941/?format=750w'),
+('The best part of working at tech Elevator is that I can carry a small part of every student in my heart.', 'Carole Ryglewicz', 'https://static1.squarespace.com/static/55ef2da9e4b03f6e1ef0cd28/t/57a4da4bbebafb5eab53f3cb/1470421591941/?format=750w'),
 ('Never give up, keep moving forward in pursuit of your dreams.', 'Kelly Brucker', 'https://static1.squarespace.com/static/55ef2da9e4b03f6e1ef0cd28/t/5977546917bffc3fac76ade9/1500992674480/de23a8c6-5860-4c7a-88a0-615207aa883b.png?format=750w'),
 ('Excitment comes from the achivement fulfillment comes from the jouney that got us there.', 'Simon Sinet', 'https://pi.tedcdn.com/r/pe.tedcdn.com/images/ted/168354_254x191.jpg?'),
 ('Fake it till you make it', 'Stacy Harrison', 'https://pbs.twimg.com/profile_images/751877895451242496/aqDcbdP8_400x400.jpg'),
