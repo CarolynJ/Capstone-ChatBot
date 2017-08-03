3845c08b6dacfb4f5ba5c3e1f4a7e9f945c4f9f1
Kevin Somerville <ksomerville0101@gmail.com>
2017-08-02 20:04:16 -0400

    modify name response

--- StudentChatBot/StudentChatBot/Dialogs/NameResponseDialog.cs
+++ StudentChatBot/StudentChatBot/Dialogs/NameResponseDialog.cs
@@ -19,21 +19,60 @@ namespace StudentChatBot.Dialogs
 
         }
 
-        private async Task NameCollected(IDialogContext context, IAwaitable<object> result)
+        private async Task NameCollected(IDialogContext context, IAwaitable<IMessageActivity> result)
         {
             var activity = await result as Activity;
             var userName = activity.Text.ToString().ToLower();
             userName = userName.Substring(0, 1).ToUpper() + userName.Substring(1);
 
-            var response = "It's nice to meet you " + userName;
-            await context.PostAsync(response);
-            Thread.Sleep(4000);
-           
-            StateClient stateClient = activity.GetStateClient();
-            BotData userData = await stateClient.BotState.GetUserDataAsync(activity.ChannelId, activity.From.Id);
-            userData.SetProperty<string>("UserName", userName);
+            var response = "";
+            if(userName == "Josh")
+            {
+                Attachment attachment = new Attachment();
+                attachment.ContentType = "image/png";
+                attachment.ContentUrl = "https://pbs.twimg.com/profile_images/658652805360689153/lRuRLtnh.jpg";
+                var message = context.MakeMessage();
+                message.Text = "Josh, huh.....any chance this is Josh Tucholski?";
 
+                message.Attachments.Add(attachment);
+
+                await context.PostAsync(message);
+            }
+            else
+            {
+                context.Wait(OtherResponse);
+                response = "It's nice to meet you " + userName;
+                await context.PostAsync(response);
+                Thread.Sleep(4000);
+
+                StateClient stateClient = activity.GetStateClient();
+                BotData userData = await stateClient.BotState.GetUserDataAsync(activity.ChannelId, activity.From.Id);
+                userData.SetProperty<string>("UserName", userName);
+
+                context.Done(true);
+            }
+
+            context.Wait(JoshResponse);
+        }
+
+        public async Task JoshResponse(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var activity = await result;
+
+            if(activity.Text.ToLower() == "yes" || activity.Text.ToLower() == "y")
+            {
+                string title = "check out this link buddy";
+                string content = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+                var markdownContent = $"[{title}]({content})";
+
+                await context.PostAsync(markdownContent);
+
+                Thread.Sleep(4000);
+                context.Done(true);
+            }
+            await context.PostAsync("Oh ok nevermind, how can I help you today?");
             context.Done(true);
+
         }
 
     }
