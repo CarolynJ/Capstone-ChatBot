2e646588c0d01fafa16ee41ffd9c9b402596c748
Carolyn Jillson <carolynj70@gmail.com>
2017-08-03 14:53:16 -0400

    added some quotes to the database

---
+++ database/SQLQuery3.sql
@@ -0,0 +1,17 @@
+insert into Motivation 
+values ('Trust the process.', 'Andria Loczi', 'https://static1.squarespace.com/static/55ef2da9e4b03f6e1ef0cd28/t/57d3125546c3c4fb955efa21/1473450614923/?format=500w'),
+('There that sould work now.', 'Josh Tucholski', 'https://static1.squarespace.com/static/55ef2da9e4b03f6e1ef0cd28/t/57a34a4bf7e0ab51c83dd878/1470841571258/?format=500w'), 
+('You cannot argue with the compiler.', 'Craig Castelaz', 'https://static1.squarespace.com/static/55ef2da9e4b03f6e1ef0cd28/t/57a36398ff7c509ef769d318/1470325664286/?format=500w'),
+('Studying at Tech Elevator is like drinking from a fire hose. Nobody leaves here dehydrated!', 'Anthony Hughes', 'https://static1.squarespace.com/static/55ef2da9e4b03f6e1ef0cd28/t/57a34d24440243b50f0b8576/1470319920350/?format=750w'),
+('The night is always darkest right before dawn', 'David Wintrich', 'https://static1.squarespace.com/static/55ef2da9e4b03f6e1ef0cd28/t/57a361c3cd0f687001676945/1470325196165/?format=750w'),
+('When the aters get rough, keep your oar in the water.', 'CB', 'https://static1.squarespace.com/static/55ef2da9e4b03f6e1ef0cd28/t/57dc62f8ebbd1a8e8340a00f/1474061055358/d9162d20-13cb-4b0c-892b-1dafb08b6b00.png?format=750w');
+
+insert into Motivation
+values('Whatever is meant to happen will happen.', 'Andria Loczi', 'https://static1.squarespace.com/static/55ef2da9e4b03f6e1ef0cd28/t/57d3125546c3c4fb955efa21/1473450614923/?format=500w'),
+('Why don''t you build a spreadsheet instead.', 'Sam Learner', 'https://static1.squarespace.com/static/55ef2da9e4b03f6e1ef0cd28/t/57bf0be8cd0f6879f01f02e4/1497033395481/68a925bf-61eb-4fed-993a-4dbf13bb899f.png?format=750w'),
+('Take a break and try some of my homemade hummus.', 'Carole Ryglewicz', 'https://static1.squarespace.com/static/55ef2da9e4b03f6e1ef0cd28/t/57a4da4bbebafb5eab53f3cb/1470421591941/?format=750w'),
+('Never give up, keep moving forward in pursuit of your dreams.', 'Kelly Brucker', 'https://static1.squarespace.com/static/55ef2da9e4b03f6e1ef0cd28/t/5977546917bffc3fac76ade9/1500992674480/de23a8c6-5860-4c7a-88a0-615207aa883b.png?format=750w'),
+('Excitment comes from the achivement fulfillment comes from the jouney that got us there.', 'Simon Sinet', 'https://pi.tedcdn.com/r/pe.tedcdn.com/images/ted/168354_254x191.jpg?'),
+('Fake it till you make it', 'Stacy Harrison', 'https://pbs.twimg.com/profile_images/751877895451242496/aqDcbdP8_400x400.jpg'),
+('It doesn''t matter how many things you need to be doing, if you learned something your time was well spent.', 'Carolyn Jillson', 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/5/000/29f/118/0fee90b.jpg');
+
